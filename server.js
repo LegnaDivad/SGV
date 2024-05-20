ec3fa303ff1f170810b8adc8933adb28e5698020
LegnaDivad <angel.pena5426@alumnos.udg.mx>
2024-05-20 06:55:52 -0600

    feat: Agregar rutas para obtener la lista de productos y consultas

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ const pool = new Pool({
 
 app.use(cors());
 app.use(express.json());
+
+// Ruta para obtener la lista de productos
+app.get('/lista_productos', async (req, res) => {
+  try {
+      const result = await pool.query(`
+          SELECT nombre, descripcion, precio, cantidad
+          FROM productos;
+      `);
+      res.json(result.rows);
+  } catch (err) {
+      console.error('Error ejecutando la consulta', err.stack);
+      res.status(500).json({ error: 'Error obteniendo los productos' });
+  }
+});
+
+
 app.get('/lista_clientes', async (req, res) => {
     try {
       const result = await pool.query(`
@@ -75,6 +91,33 @@ app.get('/ver_citas', async (req, res) => {
   } catch (err) {
     console.error('Error ejecutando la consulta', err.stack);
     res.status(500).json({ error: 'Error obteniendo las citas' });
+  }
+});
+// Ruta para obtener la lista de consultas
+app.get('/lista_consultas', async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT 
+        cl.nombre AS nombreCliente, 
+        cl.direccion, 
+        cl.telefono, 
+        cl.correo AS email, 
+        m.nombre AS nombreMascota, 
+        m.especie, 
+        m.raza, 
+        m.edad, 
+        m.genero, 
+        c.fecha, 
+        c.diagnostico, 
+        c.tratamiento
+      FROM 
+        consultas c
+     
+    `);
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error ejecutando la consulta', err.stack);
+    res.status(500).json({ error: 'Error obteniendo las consultas' });
   }
 });
 
